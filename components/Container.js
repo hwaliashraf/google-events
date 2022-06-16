4c54b21d49230535ba0e64c001b4af57f63e52d1
Ali Ashraf <ali_2961@hotmail.com>
2022-06-16 10:08:27 +0500

    Navigation: React native navigation added

---
+++ components/Container.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
+
+const Container = props => {
+  console.log(props);
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar
+        backgroundColor="transparent"
+        translucent={true}
+        barStyle="dark-content"
+      />
+      {props.children}
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: StatusBar.currentHeight,
+  },
+});
+
+export default Container;
---
+++ screens/AddEvents.js
@@ -0,0 +1,13 @@
+import {Text} from 'react-native';
+import React from 'react';
+import Container from '../components/Container';
+
+const AddEvents = () => {
+  return (
+    <Container>
+      <Text>AddEvents</Text>
+    </Container>
+  );
+};
+
+export default AddEvents;
