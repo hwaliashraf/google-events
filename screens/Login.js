7877911186e25c08c6e98e0fd99725e5a4732dda
Ali Ashraf <ali_2961@hotmail.com>
2022-06-17 11:10:43 +0500

    google authentication added

--- screens/Login.js
+++ screens/Login.js
@@ -1,11 +1,51 @@
-import {Text} from 'react-native';
-import React from 'react';
+import {View} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, {useEffect} from 'react';
 import Container from '../components/Container';
+import styles from './styles';
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+} from '@react-native-google-signin/google-signin';
+import auth from '@react-native-firebase/auth';
+import {WEB_CLIENT_ID} from '../constants';
 
-const Login = () => {
+const Login = ({navigation}) => {
+  useEffect(() => {
+    GoogleSignin.configure({
+      webClientId: WEB_CLIENT_ID,
+      offlineAccess: true,
+    });
+  }, []);
+
+  const signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const {accessToken, idToken, user} = await GoogleSignin.signIn();
+      const credential = auth.GoogleAuthProvider.credential(
+        idToken,
+        accessToken,
+      );
+      await auth().signInWithCredential(credential);
+      if (user?.id) {
+        await AsyncStorage.setItem('userInfo', JSON.stringify(user));
+        navigation.navigate('AddEvents');
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <Container>
-      <Text>Login</Text>
+      <View style={styles.loginContainer}>
+        <GoogleSigninButton
+          onPress={signIn}
+          size={GoogleSigninButton.Size.Wide}
+          color={GoogleSigninButton.Color.Dark}
+          style={styles.loginBtn}
+        />
+      </View>
     </Container>
   );
 };
--- screens/styles.js
+++ screens/styles.js
@@ -1,5 +1,12 @@
 import {StyleSheet} from 'react-native';
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  loginContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  loginBtn: {width: 200, height: 50},
+});
 
 export default styles;
